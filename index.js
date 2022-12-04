6618a04f61bfbfea83e7d1bdec8ea2dd14141dbb
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-12-04 18:01:31 +0600

    courses category data send to server

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
-
+app.get('/courses-option/:id', (req, res) => {
+    const coursesCategory = courses?.filter(course => course.course_option == req.params.id)
+    res.send(coursesCategory)
+})
 
 app.get('/courses/:id', (req, res) => {
-    const singleCourse = courses.find(course => course.id == req.params.id)
+    const singleCourse = courses?.find(course => course.id == req.params.id)
     res.send(singleCourse)
 })
 
