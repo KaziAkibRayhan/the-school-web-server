bc87af5cc0ab8f1cff108f5e358abdc940c58288
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-12-06 00:03:23 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
+
+app.use(cors())
+const coursesOption = require('./data/course.option.json')
 
 const courses = require('./data/courses.json')
 
@@ -12,7 +16,11 @@ app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
-app.get('/courses-option/:id', (req, res) => {
+app.get('/courses-category', (req, res) => {
+    res.send(coursesOption)
+})
+
+app.get('/courses-category/:id', (req, res) => {
     const coursesCategory = courses?.filter(course => course.course_option == req.params.id)
     res.send(coursesCategory)
 })
@@ -20,6 +28,11 @@ app.get('/courses-option/:id', (req, res) => {
 app.get('/courses/:id', (req, res) => {
     const singleCourse = courses?.find(course => course.id == req.params.id)
     res.send(singleCourse)
+})
+
+app.get('/courses/purchase/:id', (req, res) => {
+    const purchaseCourse = courses?.find(course => course.id == req.params.id)
+    res.send(purchaseCourse)
 })
 
 app.listen(port, () => {
